6ae018b8362790bb1fddacf70b5403f29eb78b07
eleazarc799 <eleazarc799@gmail.com>
2024-03-18 02:26:59 -0400

    Semana1 inicio

---
+++ PatronesDeDisenoSemana1/src/main/java/com/example/patronesdedisenosemana1/PatronesDeDisenoSemana1Application.java
@@ -0,0 +1,13 @@
+package com.example.patronesdedisenosemana1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesDeDisenoSemana1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesDeDisenoSemana1Application.class, args);
+	}
+
+}
