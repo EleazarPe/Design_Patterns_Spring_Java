4e16bb6fdd2a0760dd15deb782fd4709ce79dd5a
Ramon <radf0001@ce.pucmm.edu.do>
2024-03-18 23:21:06 -0400

    Singleton a Spring

--- PatronesDeDisenoSemana1/src/main/java/com/example/patronesdedisenosemana1/SingletonClase.java
+++ PatronesDeDisenoSemana1/src/main/java/com/example/patronesdedisenosemana1/SingletonClase.java
@@ -1,37 +1,19 @@
 package com.example.patronesdedisenosemana1;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
 public class SingletonClase {
 
     private String valor;
-    private static SingletonClase instanciaUnica = null;
 
-    // El constructor del Singleton debe ocultarse del código cliente. 
-    // Ya que la llamada al método getInstancia debe ser la única manera de obtener el objeto de Singleton.
-    private SingletonClase(String valor) {
+    public void setValor(String valor){
         this.valor = valor;
     }
 
-    // Este metodo estatico devuelve la misma instancia de su propia clase.
-    // Este método almacena en caché el primer objeto creado y lo devuelve en todas las llamadas siguientes.
-    public static SingletonClase getInstancia(String valor) {
-        if (instanciaUnica == null){
-            instanciaUnica = new SingletonClase(valor);
-        }
-        return instanciaUnica;
-    }
-
-    
-    // Este método no toma ningún parámetro adicional y 
-    // simplemente imprime el valor almacenado en el objeto 
-    // Singleton al que pertenece.
-    public String imprimirValor() {
-        System.out.println(valor);
+    public String getValor() {
         return valor;
-    }
-
-    // Este método toma un parámetro adicional valor y cambia el valor 
-    // almacenado en el objeto Singleton al que pertenece.
-    public void cambiarValor(String valor) {
-        this.valor = valor;
     }
 }
--- PatronesDeDisenoSemana1/src/main/java/com/example/patronesdedisenosemana1/SingletonControlador.java
+++ PatronesDeDisenoSemana1/src/main/java/com/example/patronesdedisenosemana1/SingletonControlador.java
@@ -1,5 +1,6 @@
 package com.example.patronesdedisenosemana1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,39 +11,38 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping()
 public class SingletonControlador {
 
+    private final SingletonClase singletonClase1;
+    private final SingletonClase singletonClase2;
+    
+    @Autowired
+    public SingletonControlador(SingletonClase singletonClase) {
+        this.singletonClase1 = singletonClase;
+        this.singletonClase2 = singletonClase;
+    }
 
-    // Aqui el Singleton se crea con el valor "singletonClase#1" en la primera llamada al método getInstancia, 
-    // por lo que en la segunda llamada, el condicional if (instanciaUnica == null) en el método getInstancia evalúa como falso y no 
-    // crea una nueva instancia. Por lo tanto, ambas variables singletonClase1 y singletonClase2 apuntan a la misma instancia del Singleton, 
-    // y cuando se llama al método imprimirValor en ambas variables, se imprime el mismo valor dos veces.
     @GetMapping("/singletonClase1")
     public String getSingleton(HttpServletRequest request) {
 
-        SingletonClase singletonClase1 = SingletonClase.getInstancia("singletonClase#1");
-        SingletonClase singletonClase2 = SingletonClase.getInstancia("singletonClase#2");
+        singletonClase1.setValor("singletonClase#1");
+        singletonClase2.setValor("singletonClase#2");
 
-        String cadena1 = singletonClase1.imprimirValor();
-        String cadena2 = singletonClase2.imprimirValor();
+        String cadena1 = singletonClase1.getValor();
+        String cadena2 = singletonClase2.getValor();
 
         return cadena1 + "<br>" + cadena2;
     }
 
 
-    // En este endpoint se cambia el valor para luego demostrar que ambas variables tanto 
-    // singletonClase1 y singletonClase2 utilizan la misma instancia de la clase SingletonClase
     @GetMapping("/singletonClase2")
     public String getSingletonConCambioValor(HttpServletRequest request) {
 
-        SingletonClase singletonClase1 = SingletonClase.getInstancia("singletonClase#1");
-        SingletonClase singletonClase2 = SingletonClase.getInstancia("singletonClase#2");
+        String cadena1 = singletonClase1.getValor();
+        String cadena2 = singletonClase2.getValor();
 
-        String cadena1 = singletonClase1.imprimirValor();
-        String cadena2 = singletonClase2.imprimirValor();
+        singletonClase2.setValor("singletonClase#2");
 
-        singletonClase2.cambiarValor("singletonClase#2");
-
-        String cadena3 = singletonClase1.imprimirValor();
-        String cadena4 = singletonClase2.imprimirValor();
+        String cadena3 = singletonClase1.getValor();
+        String cadena4 = singletonClase2.getValor();
 
         return String.format("singletonClase1 = %s <br> singletonClase2 = %s <br><br><br> singletonClase1 = %s <br> singletonClase2 = %s <br>", cadena1, cadena2, cadena3, cadena4);
     }
