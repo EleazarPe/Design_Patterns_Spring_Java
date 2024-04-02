b15c9cfcf660a130d996d9618e4d36ee8c9a067a
Ramon <radf0001@ce.pucmm.edu.do>
2024-04-01 20:45:34 -0400

    Dejando solamente decorator en este branch

--- decorator/src/main/java/com/example/decorator/Notification.java
+++
@@ -1,9 +0,0 @@
-package com.example.decorator;
-
-/* 
-La interfaz de Notification (Componente) define 
-la interfaz común para contenedores y objetos envueltos.
-*/
-public interface Notification {
-	public void sendMessage(String msg);
-}
